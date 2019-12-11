54464314eb8a4299ba9d49a3f3694131c3bbae1d
cmlesquivel <47956567+cmlesquivel@users.noreply.github.com>
2019-12-11 12:15:54 -0500

    Delete 101-remove_char_at.py

--- 101-remove_char_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, x):
-    new_str = ""
-
-    for n in range(len(str)):
-        if n == x:
-            continue
-        new_str = new_str + str[n]
-    return new_str
