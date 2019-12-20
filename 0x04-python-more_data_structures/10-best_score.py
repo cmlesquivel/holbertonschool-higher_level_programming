826e274187995b567ed3ca2a44c48d3b9458efe6
cmlesquivel <anayacamilo@yahoo.es>
2019-12-20 02:36:15 +0000

    function that returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max = 0
+    score = ''
+
+    if a_dictionary:
+        for clave, valor in sorted(a_dictionary.items()):
+            if valor > max:
+                score = clave
+        return score
+    else:
+        return None
