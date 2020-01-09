cc345772b5d93c31612357583b012599a3724152
cmlesquivel <anayacamilo@yahoo.es>
2020-01-09 02:29:05 +0000

    function that prints an integer at stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    is_integer = True
     try:
         print('{:d}'.format(value))
-        return is_integer
+        return True
     except ValueError as err:
         print("Exception {}".format(err), file=sys.stderr)
-        return not is_integer
+        return False
