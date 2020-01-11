492db002d3485d7f3472e822b00bb176d01fd70c
cmlesquivel <anayacamilo@yahoo.es>
2020-01-11 01:53:53 +0000

    define atribute position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,6 +9,14 @@ class Square:
             raise ValueError('size must be >= 0')
 
         self.__size = size
+
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
         self.__position = position
 
     def area(self):
@@ -43,7 +51,7 @@ class Square:
     @property
     def position(self):
         """method that returns the atribute position"""
-        return self.__size
+        return self.__position
 
     @position.setter
     def position(self, value):
