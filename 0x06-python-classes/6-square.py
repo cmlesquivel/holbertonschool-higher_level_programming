7c7383826cb69b214a55c7ca8836c5532881fd7e
cmlesquivel <anayacamilo@yahoo.es>
2020-01-11 01:59:08 +0000

    define atribute position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,6 +39,8 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        if self.__size == 0:
+            print()
         for y in range(self.__position[1]):
             print()
         for i in range(self.__size):
