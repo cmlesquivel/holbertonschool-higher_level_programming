6bb775e2f32d22bda3e171d0c533b7bab582365a
cmlesquivel <anayacamilo@yahoo.es>
2020-01-11 02:20:40 +0000

    define atribute position- fix print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,14 +41,15 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for y in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print('{}'.format(' '), end="")
-            for x in range(self.__size):
-                print('{}'.format('#'), end='')
-            print()
+        else:
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print('{}'.format(' '), end="")
+                for x in range(self.__size):
+                    print('{}'.format('#'), end='')
+                print()
 
     @property
     def position(self):
