d962a8bb6e18e712f2f3f52b05a66582301cbba3
cmlesquivel <anayacamilo@yahoo.es>
2020-01-14 00:18:54 -0500

    four test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This is a module for the test the functions
+add_integer function add
+"""
+
+
+def print_square(size):
+
+    """function that prints a square with the character #
+Args:
+    size: size
+Returns:
+    Returns nothing
+Raises:
+    TypeError: size must be an integer
+"""
+    if not isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+
+    if size == 0:
+        raise ValueError('size must be >= 0')
+
+    if size is None:
+        raise TypeError('size must be an integer')
+
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    else:
+        for x in range(size):
+            for i in range(size):
+                print('#', end='')
+            print()
