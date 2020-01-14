ad6d11cca887f96dd506ef953dd45dacc95156b2
cmlesquivel <anayacamilo@yahoo.es>
2020-01-14 00:30:09 -0500

    four test

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,7 +16,7 @@ Returns:
 Raises:
     TypeError: size must be an integer
 """
-    if not isinstance(size, float) and size < 0:
+    if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
 
     if not isinstance(size, int):
