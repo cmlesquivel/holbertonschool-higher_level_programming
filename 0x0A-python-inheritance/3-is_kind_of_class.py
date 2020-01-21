9b60fa166a8f22a71749aa20c848c4409371f130
cmlesquivel <anayacamilo@yahoo.es>
2020-01-20 20:55:49 -0500

    define if a object/class is an instance from other class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+documentation from the class is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """"returns True if the object/class is an instance from other class"""
+    return isinstance(obj, a_class)
