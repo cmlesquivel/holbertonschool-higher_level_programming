c12c12209feab9f8aea00623f483bcbf8d5250c3
cmlesquivel <anayacamilo@yahoo.es>
2020-01-20 23:56:40 -0500

    define methods area and str

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+documentation from the class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """"BaseGeometry class emply """
+
+    def area(self):
+        """"Public instance method that raises an Exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """" method that  set name and value"""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """"Rectangle class  that inherits BaseGeometry"""
+
+    def __init__(self, width, height):
+        """" method constructor"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """" define method area, return area of Rectangle """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """"return description of Rectangle """
+        return '[Rectangle] {}/{}'.format(self.__height, self.__width)
