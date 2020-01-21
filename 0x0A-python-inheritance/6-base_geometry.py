b82c8284c997ad01eb3349011d683d64d5f82621
cmlesquivel <anayacamilo@yahoo.es>
2020-01-20 22:22:15 -0500

    define that raises an Exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,4 +8,5 @@ class BaseGeometry():
     """"BaseGeometry class emply """
 
     def area(self):
+        """"Public instance method that raises an Exception"""
         raise Exception('area() is not implemented')
