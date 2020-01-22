296a9df7941460a23304ffcff6fc9312a5d846cb
cmlesquivel <anayacamilo@yahoo.es>
2020-01-21 23:06:07 -0500

    function that returns the dictionary description with simple data structure

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -3,17 +3,5 @@ import json
 
 
 def class_to_json(obj):
-    """
-    A function takes in a custom object and returns a dictionary
-    representation of the object.This dict representation includes
-    meta data such as the object's module and class names.
-    """
-
-    obj_dict = {
-        "__class__": obj.__class__.__name__,
-        "__module__": obj.__module__
-    }
-
-    obj_dict.update(obj.__dict__)
-
-    return obj_dict
+    """returns the dictionary description with simple data structure"""
+    return obj.__dict__
