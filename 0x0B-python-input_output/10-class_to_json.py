f9b3be3ed8c8691fae49d3c8b4fc28e73c2a014a
cmlesquivel <anayacamilo@yahoo.es>
2020-01-21 23:00:45 -0500

    function that returns the dictionary description with simple data structure

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -9,13 +9,11 @@ def class_to_json(obj):
     meta data such as the object's module and class names.
     """
 
-    #  Populate the dictionary with object meta data
     obj_dict = {
         "__class__": obj.__class__.__name__,
         "__module__": obj.__module__
     }
 
-    #  Populate the dictionary with object properties
     obj_dict.update(obj.__dict__)
 
     return obj_dict
