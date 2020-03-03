1e63ca5015d8dd1c6e2cd54c3341f06d2476412f
cmlesquivel <anayacamilo@yahoo.es>
2020-03-02 23:20:23 -0500

    list the number of records

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- script that lists the number of records with the same score in the table table second_table
 SELECT score, COUNT(score) AS number
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY COUNT(score) DESC;
