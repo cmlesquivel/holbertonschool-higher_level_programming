a7cc1fd94af196c33711c0b6860434a0496abc0c
cmlesquivel <anayacamilo@yahoo.es>
2020-03-03 21:29:38 +0000

    create database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
--- script that creates the MySQL server user user_0d_1
+-- script that creates the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhoyst' IDENTIFIED BY 'user_0d_2_pwd';
+-- script that creates the user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- set user to database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
