b52e56a8c646901afc73a6774973535101409ec8
cmlesquivel <anayacamilo@yahoo.es>
2020-03-03 21:17:15 +0000

    creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhoyst' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
