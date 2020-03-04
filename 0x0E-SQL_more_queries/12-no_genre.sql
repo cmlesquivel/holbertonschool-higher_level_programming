d96e9210fafa34a8af447358a1f78e2ceabaad92
cmlesquivel <anayacamilo@yahoo.es>
2020-03-04 05:00:50 +0000

    script that lists all shows contained in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.id IS NULL
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY title, genre_id;
