fb12f451df10dac82de93bf7ba5dcb7536f6dab7
cmlesquivel <1210@holbertonschool.com>
2020-03-29 21:22:41 +0000

    states beging with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that get all states
+Script that Filter states
 """
 
 import MySQLdb
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                          user=my_user, passwd=my_pasword, db=my_db)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[N]' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     result_set = cur.fetchall()
     for row in result_set:
         print(row)
