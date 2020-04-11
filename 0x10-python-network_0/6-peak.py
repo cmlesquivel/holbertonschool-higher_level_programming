b98ab1e25344c7a98e89f1f2bea439117c0f40f3
cmlesquivel <1210@holbertonschool.com>
2020-04-11 04:30:45 +0000

    function that Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 # Find a peak of list of unsorted integers
 
+
 def find_peak(list_of_integers):
     """return the peak of a list"""
 
+    max = 0
+
     if list_of_integers:
-        return (max(list_of_integers))
+        for x in list_of_integers:
+            if x > max:
+                max = x
+        return max
     else:
-        return ("None")
+        return "None"
