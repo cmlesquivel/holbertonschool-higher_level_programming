77b8284f5956e32987a766993aa5146a47f51789
cmlesquivel <1210@holbertonschool.com>
2020-04-11 03:20:15 +0000

    sends POST requets

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a POST request
-curl "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
