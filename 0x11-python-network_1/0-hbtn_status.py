b14b95373608a271a624b8264b605d7ebc686b7e
cmlesquivel <1210@holbertonschool.com>
2020-04-16 18:08:25 +0000

    python script that get of info of url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-#Python script that fetches the type and content of intranet.hbtn/io/status 
+""" Python script that fetches the type and content
+of intranet.hbtn/io/status """
 
 import urllib.request
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   data = response.read()
+    data = response.read()
 print("Body response:")
 print("\t- type: {}".format(type(data)))
 print('\t- content: {}'.format(data))
