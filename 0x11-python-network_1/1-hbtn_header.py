838224fc817e7afbbed912870aa85d7067efee6c
cmlesquivel <1210@holbertonschool.com>
2020-04-16 18:52:37 +0000

    script that get attr X-Request-Id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that get the attribute X-Request-Id of URL
+from  line command """
+
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    data = response
+print(data.getheader('X-Request-Id'))
