919444ffd8b9d3de921f84dcb3fb2d21a2f527f4
cmlesquivel <1210@holbertonschool.com>
2020-04-16 22:56:30 +0000

    fix bugs

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ Python script that send a POST request to URL from line """
 
-from urllib import parse
-from urllib import request
+import urllib.parse
+import urllib.request
 import sys
 
 if __name__ == "__main__":
     try:
         query_args = {'email': sys.argv[2]}
-        encoded_args = parse.urlencode(query_args).encode('utf-8')
-        url = sys.argv[1]
-        print(request.urlopen(url, encoded_args).read().decode('utf-8'))
+        encoded_args = urllib.parse.urlencode(query_args)
+        encoded_args = encoded_args.encode('utf-8')
+        req = urllib.request.Request(sys.argv[1], encoded_args)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
     except Exception as e:
         print(e)
