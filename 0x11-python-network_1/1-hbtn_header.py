208eef44ec0462aecbb201a632df9a41c9881e0a
cmlesquivel <1210@holbertonschool.com>
2020-04-16 20:20:41 +0000

    script that get attr X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ import sys
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.getheader('X-Request-Id'))
+            print(response.headers.get('X-Request-Id'))
     except:
         pass
