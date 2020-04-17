152f10fe005eec5c33f440cce2b7b837f31e99d8
cmlesquivel <1210@holbertonschool.com>
2020-04-17 04:59:42 +0000

    api github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,10 +7,10 @@ import sys
 if __name__ == "__main__":
 
     url = 'https://api.github.com/user'
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     try:
-        response = requests.get(url, auth=(user, passsword))
+        response = requests.get(url, auth=(username, passsword))
         my_dict = response.json()
         print("{}".format(my_dict['id']))
     except:
