4efee11e2e57588a25d9598b81dea41758b382c1
cmlesquivel <1210@holbertonschool.com>
2020-05-06 03:53:44 +0000

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that prints a square
+
+const myVar = parseInt(process.argv[2]);
+
+if (Number.isInteger(myVar)) {
+  if (myVar > 0) {
+    for (let i = 0; i < myVar; i++) {
+      console.log('x'.repeat(myVar));
+    }
+  }
+} else {
+  console.log('Missing size');
+}
