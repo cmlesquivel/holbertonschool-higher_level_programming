248f899bbdbaa78681f59d92377ee836407f8807
cmlesquivel <1210@holbertonschool.com>
2020-05-06 03:11:49 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that print into console
+
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
