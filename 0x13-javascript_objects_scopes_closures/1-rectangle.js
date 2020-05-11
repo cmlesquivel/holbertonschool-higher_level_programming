f94ab35b7f22277414dc97d1559ba35f0d8c3c77
cmlesquivel <1210@holbertonschool.com>
2020-05-11 17:46:18 -0500

    define attributes

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Define the attributes of Rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
