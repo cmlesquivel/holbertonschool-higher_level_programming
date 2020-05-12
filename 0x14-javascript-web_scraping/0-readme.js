751a0b03fbb08c0cfd7a7c831ef38e4263090237
cmlesquivel <1210@holbertonschool.com>
2020-05-12 14:24:41 -0500

    reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+
+const myFile = process.argv[2];
+
+fs.readFile(myFile, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
