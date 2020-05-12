7443b815a613409db1d348d40c30dc981bffb0a2
cmlesquivel <1210@holbertonschool.com>
2020-05-11 21:58:35 -0500

    Define the rotate and double methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Define the rotate and double methods
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    this.temp = this.height;
+    this.height = this.width;
+    this.width = this.temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
