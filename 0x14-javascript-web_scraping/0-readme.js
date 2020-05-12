dcd38f5e50aa8426c9d9ce4a2f4a3cdcd965d5a4
cmlesquivel <1210@holbertonschool.com>
2020-05-12 15:02:12 -0500

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,7 +7,7 @@ const myFile = process.argv[2];
 
 fs.readFile(myFile, 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
