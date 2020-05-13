9da07c8ac510c47371449520189631fd89771b53
cmlesquivel <1210@holbertonschool.com>
2020-05-12 22:10:30 -0500

    get content and save a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const URL = process.argv[2];
+const myFile = process.argv[3];
+
+request({
+  url: URL,
+  json: true
+}, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+
+  fs.writeFile(myFile, body, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
